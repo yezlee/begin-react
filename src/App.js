2ef6ecb9d8d5cbce69dd4513d2843a145d6ba54d
Yeseul Lee <dev.yezlee@gmail.com>
2021-06-17 19:13:30 +0900

    component / JSX / Basic react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { black } from "chalk";
+import Hello from "./Hello.js";
+import "./App.css";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* How to comment out */}
+      <Hello
+      // this is possible as well but only inside tag
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// there are two ways to make a component
+// function / class
+
+function Hello() {
+  return <div>Hello, world!</div>;
+}
+
+export default Hello;
