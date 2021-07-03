7dbd4c3b408fb84173e1adc6b628b40ecffe98df
Yeseul Lee <dev.yezlee@gmail.com>
2021-07-03 20:02:41 +0900

    how to use 'useRef'

--- src/Counter.js
+++ src/Counter.js
@@ -3,22 +3,28 @@ import React, { useState } from "react";
 
 function Counter() {
   const [number, setNumber] = useState(50);
-  //   위 코드는 우리가 number라는 상태를 만들건데 이 상태의 기본값은 0으로 하겠다. setNumber는 0이라고 적혀있는 자리의 상태를 바꿔주는 함수!이다.
+  //   위 코드는 우리가 number라는 상태를 만들건데 이 상태의 기본값은 50으로 하겠다. setNumber는 50이라고 적혀있는 자리의 상태를 바꿔주는 함수!이다.
+
+  // 설명 : useState()가 호출 됐을 때 배열을 반환하게 되는데, 첫번째 요소가 number이고 두번째 요소가 setNumber라는 배열을 반환 하는 것이다.
+  // number는 현재 상태.
+  // setNumber는 현재 상태를 업데이트 해주는 요소
+
   /*
-    const numberState = useState(0);
+    const numberState = useState(50);
+
     const number = numberState[0]; 원래는 이렇게 numberState의 첫번째 인덱스
     const setNumber = numberState[1]; 얘는 두번째 인덱스 이렇게 해줘야 하는데
-    object destructuring을 통해서 배열로 저렇게 만들어서 한번에 넣어줄수 있는것이다.
+    object destructuring을 통해서 배열로 저렇게 [number, setNumber] = useState(50); 만들어서 한번에 넣어줄수 있는것이다.
 */
 
   const onIncrease = () => {
-    setNumber(number + 1);
+    setNumber(number + 1); // setNumber는 현재 상태를 업데이트! 해주는 요소
   };
   const onDecrease = () => {
     //setNumber(number - 1);
     setNumber((prevNumber) => prevNumber - 1);
     // 이렇게 '이 값을 어떻게 업데이트 할 것이다'라는 로직이 있는 함수를 넣어줄수도 있는데 이런 함수를 '업데이트 함수' 라고 부른다.
-    // 리액트 컴포넌트를 최적화 해주는 단계에서 이렇게 하는 '함수형 업데이트'가 필요하다.
+    // 지금은 이해가 안가도, 리액트 컴포넌트를 최적화 해주는 단계에서 이렇게 하는 '함수형 업데이트'가 필요하다. 라고만 기억하자.
   };
   return (
     <div>
--- src/InputSample.js
+++ src/InputSample.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
   const [inputs, setInputs] = useState({
@@ -6,18 +6,14 @@ function InputSample() {
     alias: "",
   });
 
+  const nameInput = useRef();
   const { name, alias } = inputs;
 
+  // 여기서 e(이벤트객체 : 매개변수)가 의미 하는 것은, 아래 input에서 수정이벤트가 발생했을때 그 내용이 파라미터로 들어와서 사용할수 있게 된다.
+  // 만약에 아래에서 console.log(e.target) 이라고 찍어보면 input, 즉 이벤트가 발생한 돔에 대한 정보를 갖고 있다.
+  // 그래서 그 인풋에 있는 내용을 갖고 오고 싶으면 e.targert.value를 하면 인풋에 입력되는 내용이 나온다.
   const onChange = (e) => {
     const { name, value } = e.target;
-
-    // const nextInputs = {
-    //   ...inputs,
-    //   [name]: value,
-    // };
-
-    // nextInputs[name] = value;
-
     setInputs({
       ...inputs,
       [name]: value,
@@ -29,11 +25,18 @@ function InputSample() {
       name: "",
       alias: "",
     });
+    nameInput.current.focus();
   };
 
   return (
     <div>
-      <input name="name" placeholder="name" onChange={onChange} value={name} />
+      <input
+        name="name"
+        placeholder="name"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
       <input
         name="alias"
         placeholder="alias"
