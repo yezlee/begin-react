88f453d54627177767989eb89c444acf1185165e
Yeseul Lee <dev.yezlee@gmail.com>
2021-07-05 21:32:52 +0900

    How to avoid re-render  using useRef?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React, { useRef } from "react";
 import UserList from "./UserList";
 
 function App() {
-  return <UserList />;
+  const users = [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "heemong",
+      email: "heemong9@gmail.com",
+    },
+    {
+      id: 3,
+      username: "devyez",
+      email: "dev.yezlee@gmail.com",
+    },
+  ];
+
+  const nextId = useRef(4);
+  // 위에 아이디가 3까지 있으니까 4로 해주고
+  // 담에 또 새로운 아이디, nextId가 생기는거를 위해서 함수를 만들거
+  const onCreate = () => {
+    console.log(nextId.current); //4
+    //새로 만들때마다 현재 아이디가 뭔지 알고싶음 위와같은 코드를 작성하고
+    nextId.current += 1;
+    //이렇게 하면 onCreate() 함수가 실행될때마다 현재 아이디한테 1을 더해주니까.
+
+    //nextId를 useRef로 관리하는 이유는 새로운 값이 입력될때마다 업데이트 되서 리렌더링 할 필요가 없어서 useRef를 쓴다.
+    //useState로 써도 상관은 없는데, 굳이 왜? 이런거지. 리렌더링 할 필요가 없는데.
+
+    // 정리 : useRef는 특정 돔을 선택할 떄도 ref속성을? 이용해서 사용할 수도 있지만,
+    // 컴포넌트가 리렌더링돼도 기억하고 싶은 값이 있을때도 useRef를 쓴다.
+  };
+
+  return <UserList users={users} />;
+  // 이렇게 하면 위에 적은 users를 UserList 컴포넌트에 전달하는 것임
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -10,8 +10,10 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
+function UserList({ users }) {
+  /*
+    원래 여기에 이거 있었는데 useRef사용하려고 이거 app.js로 이동
+      const users = [
     {
       id: 1,
       username: "velopert",
@@ -29,6 +31,8 @@ function UserList() {
     },
   ];
 
+
+    */
   return (
     /*
     방법 3.
@@ -76,3 +80,20 @@ function UserList() {
 }
 
 export default UserList;
+
+/*
+useRef로 컴포넌트 안의 변수 만들기
+예를 들어서 컴포넌트 내부에서 let 키워드를 사용해서 어떤 변수를 선언하다고 가정해보자, 이러면 다음에 리렌더링 할때 이 변수 값은 초기화가 된다.
+만약에 계속 유지하고 싶은 어떤 값을 관리하려면 useState()를 사용해야 하는데, useState()는 상태를 바꾸게 되면 컴포넌트가 리렌더링 된다.
+하지만 값을 바꿨을때 굳이 리렌더링 할 필요 없을 경우가 있다.
+이 때는 useRef()를 사용한다!!!
+ref를 사용해야할때 useRef()를 사용한다고 배웠는데 이처럼 꼭 어떤 특정한 돔을 선택할 때만 사용하는게 아니고,
+컴포넌트가 리렌더링 될 때마다 계속 기억할 수 있는 어떤 값을 관리할 수 있는 역할도 한다.
+
+주로 setTimeout, setInterval의 id를 가져올 때, 외부라이브러리를 사용하여 생성된 인스턴스를 담을때라던지,
+Scroll 위치를 알고있어야할때 등등..
+
+기억해야할것은
+useRef()로 관리하는 값은 바뀌어도 컴포넌트가 리렌더링 되지 않는다는것을 알아두자!!
+
+ */
