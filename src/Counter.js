7585b09566edee24381e56ee4d055f6bbe38f442
Yeseul Lee <dev.yezlee@gmail.com>
2021-07-26 19:01:23 +0900

    Basic concept of useReducer()

--- src/Counter.js
+++ src/Counter.js
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
-// 이렇게 하면 react에서 useState라는 함수를 불러오는 것
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  // 현재 상태와 액션객체를 파라미터로 받아와서 새로운 상태를 반환해주는 형태를 갖추고 있어야 한다.
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      // return state; 이렇게 해줘도 되고 에러를 던져도 되고
+      throw new Error("Unhandled action");
+  }
+}
 
 function Counter() {
-  const [number, setNumber] = useState(50);
-  //   위 코드는 우리가 number라는 상태를 만들건데 이 상태의 기본값은 50으로 하겠다. setNumber는 50이라고 적혀있는 자리의 상태를 바꿔주는 함수!이다.
-
-  // 설명 : useState()가 호출 됐을 때 배열을 반환하게 되는데, 첫번째 요소가 number이고 두번째 요소가 setNumber라는 배열을 반환 하는 것이다.
-  // number는 현재 상태.
-  // setNumber는 현재 상태를 업데이트 해주는 요소
-
-  /*
-    const numberState = useState(50);
-
-    const number = numberState[0]; 원래는 이렇게 numberState의 첫번째 인덱스
-    const setNumber = numberState[1]; 얘는 두번째 인덱스 이렇게 해줘야 하는데
-    object destructuring을 통해서 배열로 저렇게 [number, setNumber] = useState(50); 만들어서 한번에 넣어줄수 있는것이다.
-*/
+  // reducer라는 함수를 만들고 나서, useReducer를 사용할건데
+  // useReducer를 사용할 때 첫번째로는 현재의 상태(여기서는 number)가 오고, 두번째 값으로는 dispatch라는 함수가 온다.
+  // dispatch는 보내다라는 뜻을 가지고 있는데 여기서는 액션을 발생시킨다. 라고 이해하면 된다.
+  const [number, dispatch] = useReducer(reducer, 0);
+  // 첫번째 파라미터값은 우리가 만든 reducer함수, 두번째 값으로는 useReducer에서 사용하고 싶은 초기값/기본값을 넣어주면된다.
 
   const onIncrease = () => {
-    setNumber(number + 1); // setNumber는 현재 상태를 업데이트! 해주는 요소
+    dispatch({
+      type: "INCREMENT",
+    });
   };
   const onDecrease = () => {
-    //setNumber(number - 1);
-    setNumber((prevNumber) => prevNumber - 1);
-    // 이렇게 '이 값을 어떻게 업데이트 할 것이다'라는 로직이 있는 함수를 넣어줄수도 있는데 이런 함수를 '업데이트 함수' 라고 부른다.
-    // 지금은 이해가 안가도, 리액트 컴포넌트를 최적화 해주는 단계에서 이렇게 하는 '함수형 업데이트'가 필요하다. 라고만 기억하자.
+    dispatch({
+      type: "DECREMENT",
+    });
   };
   return (
     <div>
@@ -37,6 +41,24 @@ function Counter() {
 
 export default Counter;
 
+/*
+    useReducer()
+    (이전에 우리가 컴포넌트 상태를 업데이트 해야할 때는, useState()를 사용해서 새로운 상태를 설정했는데)
+    둘이 어떤 차이가 있냐면, 
+    일단 useState()에서는 설정하고 싶은 다음 상태를 직접 지정해주는 방식으로 상태를 업데이트하는 반면에,
+    useRecucer()는 action이라는 객체를 기반으로 상태를 업데이트 한다. 여기서 action객체라는 것은 업데이트 할 때 참조하는 객체이다.
+    액션안에 디스패치가 있나?
+    dispatch({
+      type : 'INCREMENT', - type이라는 값을 사용해서 어떤 업데이트를 진행할건지 명시를 할 수 있고
+      diff : 4  - 업데이트할때 필요한 참조하고 싶은 다른 값이 있다면 이 객체안에 넣을 수도 있다.
+    })
+
+    이 useReducer라는 hook함수를 사용하면 컴포넌트 상태 업데이트 로직을, 컴포넌트에서 분리 시킬수가 있다. 
+    즉 컴포넌트 밖으로 분리가 가능하다. 심지어 다른 파일에 작성 후 불러와서 사용할 수도 있다.
+
+    여기서 reducer라는 개념은 '상태를 업데이트 하는 함수'이다.
+
+*/
 /*
     useState()라는 함수를 통해서 바뀌는 값을 관리하고
     기본값은 useState()함수의 파라미터로 넘겨주면 된다.
